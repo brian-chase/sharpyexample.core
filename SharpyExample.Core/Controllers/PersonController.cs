260f91ce93897660dfc8c056e61aa5256831096b
Brian Chase <brian.chase@zoominfo.com>
2021-05-19 15:24:15 -0400

    Added samples

---
+++ SharpyExample.Core/Controllers/PersonController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SharpyExample.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Bob", "Jim" };
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Bob";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
