cc753150557f0c0cfdd801a121b21733d7ac6de2
Brian Chase <brian.chase@zoominfo.com>
2021-06-18 14:31:21 -0400

    Added Dave (#1)
    * Added Dave
    
    * Added .net framework 47 project and netstandard20 project to see if reference issues persist
    
    * Added things

---
+++ FrameworkWeb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FrameworkWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FrameworkWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FrameworkWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
