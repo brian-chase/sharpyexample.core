3a96933c43820bc68a413decd7b757c325026a21
Brian Chase <brian.chase@zoominfo.com>
2021-07-01 12:13:31 -0400

    Added new test library with project reference

---
+++ StandardClassLibraryTests/Services/PersonServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace StandardClassLibrary.Services.Tests
+{
+    [TestFixture()]
+    public class PersonServiceTests
+    {
+        [Test()]
+        public void PeopleTest()
+        {
+            var svc = new PersonService();
+            var list = svc.People(12);
+            Assert.That(list.Count(), Is.EqualTo(12));
+        }
+    }
+}
